9c2e38288684e8a0d2a5e48f65502261b1dcbd4d
kobakobashu <kobaya4@fuji.waseda.jp>
2023-04-06 14:26:24 +0900

    add chapter 3.2

--- monkey/code/code.go
+++ monkey/code/code.go
@@ -14,6 +14,9 @@ type Definition struct {
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
 	OpAdd:      {"OpAdd", []int{}},
+	OpSub:      {"OpSub", []int{}},
+	OpMul:      {"OpMul", []int{}},
+	OpDiv:      {"OpDiv", []int{}},
 	OpPop:      {"OpPop", []int{}},
 }
 
@@ -32,6 +35,9 @@ type Opcode byte
 const (
 	OpConstant Opcode = iota
 	OpAdd
+	OpSub
+	OpMul
+	OpDiv
 	OpPop
 )
 
