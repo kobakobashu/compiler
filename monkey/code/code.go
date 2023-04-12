5da99e21419f85958b7c44c7334fdb9cca593e3e
kobakobashu <kobaya4@fuji.waseda.jp>
2023-04-12 09:35:22 +0900

    add chapter 5.2

--- monkey/code/code.go
+++ monkey/code/code.go
@@ -28,6 +28,8 @@ var definitions = map[Opcode]*Definition{
 	OpJumpNotTruthy: {"OpJumpNotTruthy", []int{2}},
 	OpJump:          {"OpJump", []int{2}},
 	OpNull:          {"OpNull", []int{}},
+	OpGetGlobal:     {"OpGetGlobal", []int{2}},
+	OpSetGlobal:     {"OpGetGlobal", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -59,6 +61,8 @@ const (
 	OpJumpNotTruthy
 	OpJump
 	OpNull
+	OpGetGlobal
+	OpSetGlobal
 )
 
 func Make(op Opcode, operands ...int) []byte {
