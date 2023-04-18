0f4c3c3adac89c2d9fdac7acfaadb55897143452
kobakobashu <kobaya4@fuji.waseda.jp>
2023-04-18 17:13:27 +0900

    add a feature to compile builtin func

--- monkey/code/code.go
+++ monkey/code/code.go
@@ -38,6 +38,7 @@ var definitions = map[Opcode]*Definition{
 	OpReturn:        {"OpReturn", []int{}},
 	OpGetLocal:      {"OpGetLocal", []int{1}},
 	OpSetLocal:      {"OpSetLocal", []int{1}},
+	OpGetBuiltin:    {"OpGetBuiltin", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -79,6 +80,7 @@ const (
 	OpReturn
 	OpGetLocal
 	OpSetLocal
+	OpGetBuiltin
 )
 
 func Make(op Opcode, operands ...int) []byte {
